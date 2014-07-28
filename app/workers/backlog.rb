64bc4235314de052892797aad7b0a6ad53ff2e93
Zach Pflederer <zpfled@gmail.com>
2014-07-28 09:56:53 -0500

    clean up

--- app/workers/backlog.rb
+++ app/workers/backlog.rb
@@ -9,13 +9,8 @@ class Backlog
   def update_stars(original_tweet_params)
   	if Tweet.find_by_twitter_id(original_tweet_params["id"])
   		db_tweet = Tweet.find_by_twitter_id(original_tweet_params["id"])
-  		p "DB TWEET ID: #{db_tweet.id} ================================"
-      p "original_tweet: #{original_tweet_params.inspect}"
-      # db_tweet.update_attribute('stars', original_tweet_params["stars"])
-      p db_tweet
-
       db_tweet.stars = original_tweet_params['stars']
-  		p "ZOMG it saved? #{db_tweet.save}"
+  		db_tweet.save
   	end
   end
 
